3d2e314b192e4cad70bc9d984916f400174accd8
lelekaspb <bov-1986@yandex.ru>
2023-02-05 15:13:56 +0100

    revert to ddd4a676457e277856f4e41e31772d90975035d6
    This reverts commit ddd4a676457e277856f4e41e31772d90975035d6.

--- server.js
+++ server.js
@@ -185,9 +185,8 @@ app.put("/destinations/:id", async (req, res) => {
   destination.country = req.body.country;
   destination.location = req.body.location;
   destination.description = req.body.description;
-  if (req.files && req.files.picture) {
-    destination.picture = req.files.picture.name;
-  }
+  destination.picture =
+    req.files && req.files.picture ? req.files.picture.name : "";
 
   try {
     const savedDestination = await destination.save();
